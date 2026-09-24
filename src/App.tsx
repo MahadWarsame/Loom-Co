112b84c0b5622ca4dc9a86c8299b3b1e247d94c8
MahadWarsame <50800411+MahadWarsame@users.noreply.github.com>
2026-09-24 22:43:01 +0200

    Add shopping cart route

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { Shop } from "./Pages/Shop";
 import { ProductPage } from "./Pages/Product";
 import { NotFound } from "./Pages/NotFound";
 import { InfoPage } from "./Pages/Info";
+import { CartPage } from "./Pages/Cart";
 
 export default function App() {
   return (
@@ -15,6 +16,7 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/shop" element={<Shop />} />
+          <Route path="/cart" element={<CartPage />} />
           <Route path="/product/:slug" element={<ProductPage />} />
           <Route path="/info/:page" element={<InfoPage />} />
           <Route path="*" element={<NotFound />} />
