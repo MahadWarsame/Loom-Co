9aaaddc08a156269d366c0d4bef8ec5caeb743e4
MahadWarsame <50800411+MahadWarsame@users.noreply.github.com>
2026-09-24 22:50:13 +0200

    Add payment result routes

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import { NotFound } from "./Pages/NotFound";
 import { InfoPage } from "./Pages/Info";
 import { CartPage } from "./Pages/Cart";
 import { CheckoutPage } from "./Pages/Checkout";
+import { PaymentResultPage } from "./Pages/PaymentResult";
 
 export default function App() {
   return (
@@ -19,6 +20,8 @@ export default function App() {
           <Route path="/shop" element={<Shop />} />
           <Route path="/cart" element={<CartPage />} />
           <Route path="/checkout" element={<CheckoutPage />} />
+          <Route path="/checkout/success" element={<PaymentResultPage />} />
+          <Route path="/checkout/cancelled" element={<PaymentResultPage cancelled />} />
           <Route path="/product/:slug" element={<ProductPage />} />
           <Route path="/info/:page" element={<InfoPage />} />
           <Route path="*" element={<NotFound />} />
